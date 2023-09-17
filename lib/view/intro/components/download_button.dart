82449595bae8f64729991483c8ba2eaaff6aa342
smit aryal <smit1aryal@gmail.com>
2023-09-17 08:34:27 +0545

    readme.md deleted

--- lib/view/intro/components/download_button.dart
+++ lib/view/intro/components/download_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:flutter/foundation.dart' show kIsWeb;
 import 'dart:html' as html;
 
 import '../../../res/constants.dart';
